54f7f7170d5b5ae8f0ffeb042e65d6c59978bf03
Bhairav Mehta <bhairavm@umich.edu>
2018-02-05 17:35:55 -0500

    colors

--- snake.py
+++ snake.py
@@ -6,6 +6,12 @@ from copy import deepcopy
  
 SPRITE_SIZE = 22
 
+PLAYER_COLORS = [
+    (0, 255, 0),
+    (0, 0, 255),
+    (255, 0, 255)
+]
+
 class Fruit:
     global SPRITE_SIZE
     step = SPRITE_SIZE
@@ -50,7 +56,7 @@ class Player:
        self.length = length
        self.x = [x]
        self.y = [y]
-       for i in range(0,2000):
+       for i in range(0,2000): 
            self.x.append(-1)
            self.y.append(-1)
  
@@ -105,29 +111,19 @@ class Player:
  
     def draw(self, surface, image):
         for i in range(0,self.length):
-            surface.blit(image, (self.x[i],self.y[i])) 
- 
-class Game:
-    def isCollision(self, x1, y1, x2, y2, block_size):
-        if x1 >= x2 and x1 <= x2 + block_size:
-            if y1 >= y2 and y1 <= y2 + block_size:
-                return True
-
-        return False
- 
+            surface.blit(image, (self.x[i], self.y[i])) 
+  
 class App:
-    windowWidth = 616
-    windowHeight = 616
+    window_dimension = 616
     player = 0
     apple = 0
     global SPRITE_SIZE
  
-    def __init__(self, num_players=2, num_fruits=2):
+    def __init__(self, num_players=3, num_fruits=2):
         self._running = True
         self._display_surf = None
         self._image_surf = None
         self._apple_surf = None
-        self.game = Game()
         self.players = []
         self.fruits = []
         self.num_players = num_players
@@ -137,11 +133,12 @@ class App:
                 x = 22 * (p + 1)
                 y = 22 * (p + 1)
 
-            if p == 1:
+            else:
                 x = 22 * (p + 10)
                 y = 22 * (1)
             
             player = Player(3, x, y)
+            player.color_idx = p % len(PLAYER_COLORS)
 
             if p == 1:
                 player.direction = 1
@@ -153,29 +150,37 @@ class App:
         for f in range(num_fruits):
             self.fruits.append(Fruit(randint(1, 12), randint(1, 12)))
 
-        self.wall = Wall(self.windowHeight, self.windowWidth)
+        self.wall = Wall(self.window_dimension, self.window_dimension)
 
         self.sprite_size = SPRITE_SIZE
  
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+        self._display_surf = pygame.display.set_mode((self.window_dimension, self.window_dimension), pygame.HWSURFACE)
 
         self._running = True
 
         self._player_surfs = []
-        for p in self.players:
+
+        global PLAYER_COLORS
+        for i, p in enumerate(self.players):
             image_surf = pygame.Surface([self.sprite_size - 4, self.sprite_size - 4])
-            image_surf.fill((0, 255, 0))
-
+            image_surf.fill(PLAYER_COLORS[i % len(PLAYER_COLORS)])
             self._player_surfs.append(image_surf)
-
 
         self._fruit_surf = pygame.Surface([self.sprite_size - 4, self.sprite_size - 4])
         self._fruit_surf.fill((255, 0, 0))
 
         self._wall_surf = pygame.Surface([self.sprite_size - 4, self.sprite_size - 4])
-        self._wall_surf.fill((255, 0, 255))
+        self._wall_surf.fill((255, 255, 255))
+
+
+    def collided(self, x1, y1, x2, y2, block_size):
+        if x1 >= x2 and x1 <= x2 + block_size:
+            if y1 >= y2 and y1 <= y2 + block_size:
+                return True
+
+        return False
  
     def on_event(self, event):
         if event.type == QUIT:
@@ -188,28 +193,28 @@ class App:
             for other_player_idx in range(idx + 1, len(self.players)):
                 other_player = self.players[other_player_idx]
                 for idx_player_len in range(0, other_player.length):
-                    if self.game.isCollision(p.x[0], p.y[0], other_player.x[idx_player_len], other_player.y[idx_player_len], 20):
+                    if self.collided(p.x[0], p.y[0], other_player.x[idx_player_len], other_player.y[idx_player_len], 20):
                         self.killed[idx] = True
                         self.killed[other_player_idx] = True
 
             # does snake eat apple?
             for i in range(0, p.length):
                 for f in self.fruits:
-                    if self.game.isCollision(f.x, f.y, p.x[i], p.y[i], 19):
+                    if self.collided(f.x, f.y, p.x[i], p.y[i], 19):
                         f.x = randint(2,9) * SPRITE_SIZE
                         f.y = randint(2,9) * SPRITE_SIZE
                         p.length = p.length + 1
      
             # does snake collide with itself?
             for i in range(2, p.length):
-                if self.game.isCollision(p.x[0], p.y[0], p.x[i], p.y[i], 19):
+                if self.collided(p.x[0], p.y[0], p.x[i], p.y[i], 19):
                     self.killed[idx] = True
 
             # going off the edge
             if p.x[0] < self.sprite_size or p.y[0] < self.sprite_size:
                 self.killed[idx] = True
 
-            if p.x[0] > self.windowWidth - self.sprite_size or p.y[0] > self.windowHeight - self.sprite_size:
+            if p.x[0] > self.window_dimension - self.sprite_size or p.y[0] > self.window_dimension - self.sprite_size:
                 self.killed[idx] = True
         
         players_new = []
@@ -218,6 +223,7 @@ class App:
             if not self.killed[idx]:
                 players_new.append(self.players[idx])
 
+        print(self.killed)
         self.players = deepcopy(players_new)
         self.num_players = len(self.players)
         self.killed = [False] * self.num_players
@@ -233,7 +239,7 @@ class App:
             f.draw(self._display_surf, self._fruit_surf)
 
         for i, p in enumerate(self.players):
-            p.draw(self._display_surf, self._player_surfs[i])
+            p.draw(self._display_surf, self._player_surfs[p.color_idx])
 
         self.wall.draw(self._display_surf, self._wall_surf)
 
@@ -272,7 +278,8 @@ class App:
             self.on_loop()
             self.on_render()
  
-            time.sleep (50.0 / 1000.0);
+            # time.sleep (50.0 / 1000.0);
+
         self.on_cleanup()
  
 if __name__ == "__main__" :
